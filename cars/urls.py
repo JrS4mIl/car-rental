abd7a5bdc443d55eae0f046a477e0752323daf03
Yesil <yesil@gmail.com>
2023-11-22 15:33:50 +0300

    feat: paginator

--- cars/urls.py
+++ cars/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import cars
+from .views import cars,car_detail
 urlpatterns = [
-    path('',cars,name='cars')
+    path('',cars,name='cars'),
+    path('/<int:id>',car_detail,name='car_detail')
 
 
 ]

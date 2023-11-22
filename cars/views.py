e06d5a98ad74608fc8b08b370f84838a8a505d3f
Yesil <yesil@gmail.com>
2023-11-22 16:28:44 +0300

    feat: search start

--- cars/views.py
+++ cars/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Car
-from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 
 # Create your views here.
 def cars(request):
     cars = Car.objects.order_by('-created_date')
-    paginator=Paginator(cars,3)
-    page=request.GET.get('page')
-    paged_cars=paginator.get_page(page)
+    paginator = Paginator(cars, 3)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
     context = {
         'cars': paged_cars
     }
@@ -20,3 +21,7 @@ def car_detail(request, id):
         'single_car': single_car
     }
     return render(request, 'cars/car-details.html', context)
+
+
+def search(request):
+    return render(request,'cars/search.html')
